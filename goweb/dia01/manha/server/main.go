e42f0d3adeec5830fabf3188e877ce66bae57057
Lucas Alves <lukasalves271@gmail.com>
2025-06-06 12:14:15 -0300

    feat: add class resources

--- goweb/dia01/manha/server/main.go
+++ goweb/dia01/manha/server/main.go
@@ -17,12 +17,18 @@ func main() {
 
 	rt.Use(middleware.Heartbeat(""))
 
+	// rt.Route("/api/v1", func(r chi.Router) {
+	// 	r.Use(middleware.Logger)
+	// })
+
 	// -> endpoints
-	rt.With(middleware.Logger).Get("/hello-world", func(w http.ResponseWriter, r *http.Request) {
-		// set code and body
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World!"))
-	})
+	rt.
+		// With(middleware.Logger).
+		Get("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+			// set code and body
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("Hello World!"))
+		})
 	// run
 	if err := http.ListenAndServe(":8080", rt); err != nil {
 		panic(err)
